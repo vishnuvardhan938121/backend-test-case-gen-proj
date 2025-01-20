f9945701d0c8cf5914df5e0f89e70488d5abb1be
vishnuvardhan938121 <147582100+vishnuvardhan938121@users.noreply.github.com>
2025-01-20 23:46:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import random
 from testgenerator import generate_test_cases  # Import the generate_test_cases function
 
 app = Flask(__name__)
@@ -17,15 +16,13 @@ def generate_test_cases_route():
         if not data:
             return jsonify({"error": "Invalid request, no JSON payload provided"}), 400
         
-        code = data.get('code', '').strip()
         algorithm = data.get('algorithm_name', '').strip()
-        language = data.get('language', '').strip()
 
-        if not code or not algorithm or not language:
-            return jsonify({"error": "Missing required fields: code, algorithm_name, or language"}), 400
+        if not algorithm:
+            return jsonify({"error": "Missing required field: algorithm_name"}), 400
 
         # Use the generate_test_cases function from testgenerator.py
-        test_cases = generate_test_cases(algorithm_type="sorting", algo_name=algorithm)
+        test_cases = generate_test_cases(algo_type="sorting", algo_name=algorithm)
 
         return jsonify({"test_cases": test_cases})
 
